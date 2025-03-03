bd52b2a203faa3a34939a457b00c7674fb372587
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-03-03 10:45:05 -0800

    Added persistent highlighting

--- src/highlight.js
+++ src/highlight.js
@@ -1,6 +1,10 @@
 let parrot_highlightedElement = undefined;
 
-document.addEventListener("mouseover", (event) => {
+const display = document.createElement("div")
+display.id = "idDisplay";
+document.body.appendChild(display);
+
+const handleHighlight = (event) => {
     // Remove current highlight
     if (parrot_highlightedElement) {
         parrot_highlightedElement.classList.remove("hover-highlight");
@@ -10,13 +14,21 @@ document.addEventListener("mouseover", (event) => {
     // Highlight new element
     parrot_highlightedElement = event.target;
     parrot_highlightedElement.classList.add("hover-highlight");
-    console.log("Content highlighted", parrot_highlightedElement);
-});
 
-document.addEventListener("mouseout", () => {
+    const elemRect = parrot_highlightedElement.getBoundingClientRect();
+    const displayDiv = document.getElementById("idDisplay");
+    displayDiv.innerText = `id: ${parrot_highlightedElement.id}\nclasses: ${parrot_highlightedElement.classList}`;
+    displayDiv.style.top = elemRect.top + "px";
+    displayDiv.style.left = elemRect.left + "px";
+}
+
+const handleUnhighlight = () => {
     // Remove highlight on mouse out
     if (parrot_highlightedElement) {
         parrot_highlightedElement.classList.remove("hover-highlight");
         parrot_highlightedElement = null;
     }
-});
+}
+
+document.addEventListener("mouseover", handleHighlight);
+document.addEventListener("mouseout", handleUnhighlight);
